5d6563c01220c8cf64e9a8d221a30aa07687676c
swapnil ingle <ingleswapnil2004@gmail.com>
2025-01-04 18:28:57 +0530

    creating SSO dashboard

--- src/components/custom/card.tsx
+++ src/components/custom/card.tsx
@@ -1,32 +1,53 @@
-import React from 'react'
-import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
-} from "@/components/ui/card"
+/* eslint-disable @next/next/no-img-element */
+import { Card, CardHeader, CardTitle, CardDescription, CardContent, CardFooter } from "@/components/ui/card";
 
-
-function CardComp() {
+const InfoCard = (
+    {
+        title,
+        description,
+        link,
+        imageUri,
+        githubLink
+    }: {
+        title: string,
+        description: string,
+        link: string,
+        imageUri: string,
+        githubLink: string
+    }
+) => {
     return (
-        <Card
-            className='bg-gray-100 dark:bg-gray-800 w-max m-1   shadow-slate-500 shadow-inner min-w-64 min-h-80 flex flex-col justify-center items-center text-center'
-        >
-            <CardHeader>
-                <CardTitle>Card Title</CardTitle>
-                <CardDescription>Card Description</CardDescription>
+        <Card className="w-max max-w-sm p-6 shadow-lg rounded-lg border border-gray-200">
+            <CardHeader className="mb-4">
+                <CardTitle className="  font-semibold text-slate-600 text-3xl">
+                    <img src={imageUri} alt={title} className="w-12 h-12 rounded-full inline-block mr-2 border border-solid border-slate-800 p-1" />
+                    {title}
+                </CardTitle>
+                <CardDescription className="text-gray-600">
+                    {description}
+                </CardDescription>
             </CardHeader>
-            <CardContent>
-                <p>Card Content</p>
-            </CardContent>
-            <CardFooter>
-                <p>Card Footer</p>
+            <CardFooter className="flex justify-end">
+                <a
+                    href={link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:ring focus:ring-blue-300"
+                >
+                    Visit
+                </a>
+                <a
+                    href={githubLink}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600 focus:ring focus:ring-blue-300"
+                >
+                    Visit Github
+                </a>
+                
             </CardFooter>
         </Card>
+    );
+};
 
-    )
-}
-
-export default CardComp
+export default InfoCard;
