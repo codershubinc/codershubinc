72fc3a5c7f999cb91e8a1f8498053ea4d57c0cce
Swapnil Ingle <ingleswapnil2004@gmail.com>
2025-02-11 17:03:10 +0530

    converting this repo to only profile repo

--- next.config.mjs
+++
@@ -1,9 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    images: {
-        domains: ['res.cloudinary.com', "github.com"],
-    },
-};
-// add image domain to next config
-
-export default nextConfig;
